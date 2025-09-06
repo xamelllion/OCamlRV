f473d0fbc23f5b22c763c858d8282b6db247145f
Viacheslav Sidorov <me@xamelllion.ru>
2025-09-06 11:48:00 +0300

    feat: update example.ml

--- bin/example.ml
+++ bin/example.ml
@@ -1,13 +1,1 @@
-let rec list_iter l n =
-  if n = 0
-  then ()
-  else (
-    match l with
-    | [] -> ()
-    | [ x ] -> print_int x
-    | x :: xs ->
-      let () = print_int x in
-      list_iter xs (n - 1))
-;;
-
-list_iter [ 1; 2; 3; 4; 5 ] 5
+let _ = print_endline "Hello world!"
